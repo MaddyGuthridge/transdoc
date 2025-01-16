66e1132020f6d293b8c343969065bd732daf953e
Maddy Guthridge <hello@maddyguthridge.com>
2025-01-17 00:21:44 +1100

    Handler API: Use regex to match files, instead of a list of file extensions

--- transdoc/handlers/api.py
+++ transdoc/handlers/api.py
@@ -3,9 +3,14 @@
 
 API definition for Transdoc handler modules.
 """
+from collections.abc import Sequence
+import re
 from typing import Protocol
 from pathlib import Path
 from transdoc.__transformer import TransdocTransformer
+
+
+RegexPattern = re.Pattern[str] | str
 
 
 class TransdocHandler(Protocol):
@@ -13,12 +18,12 @@ class TransdocHandler(Protocol):
     A language handler plugin for transdoc.
     """
 
-    def get_file_extensions(self) -> list[str]:
+    def get_file_matchers(self) -> Sequence[RegexPattern]:
         """
         Returns the list of file extensions that this handler supports.
 
-        This should be a list of strings, where each string is a file
-        extension, excluding the leading dot.
+        This should be a sequence of regular expressions which match the
+        desired files.
 
         Returns:
             list[str]: supported file extensions (eg `['txt', 'md'])
@@ -36,8 +41,8 @@ class TransdocHandler(Protocol):
         transformed output into the file at `out_path`.
 
         Args:
-            transformer (TransdocTransformer): use `transformer.apply` on any
-            rule calls.
+            transformer (TransdocTransformer): use `transformer.transform` on
+            any strings where rules should be applied.
             in_path (Path): path to input file
             out_path (Path): path to output file
         """
