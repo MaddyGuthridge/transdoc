073f33793db3524461e5c51259c777b934b988b1
Maddy Guthridge <hello@maddyguthridge.com>
2025-01-24 23:21:30 +1100

    Add function for simple string transformation

--- transdoc/__init__.py
+++ transdoc/__init__.py
@@ -14,9 +14,31 @@ __all__ = [
     "TransdocRule",
 ]
 
+from io import StringIO
 from .__rule import TransdocRule
 from .__consts import VERSION as __version__
 from .__transformer import TransdocTransformer
-from .handlers import TransdocHandler
+from .handlers import TransdocHandler, PlaintextHandler
 from .__transform_tree import transform_tree
 from .__transform_file import transform_file
+
+
+def transform(
+    transformer: TransdocTransformer,
+    input: str,
+    path: str = "<string>",
+) -> str:
+    """
+    Transform the given input string.
+
+    Args:
+        transformer (TransdocTransformer): Transformer with all desired rules
+        input (str): input string to transform
+        path (str, optional): name of input string to use when reporting errors
+    """
+    handlers = [PlaintextHandler()]
+    in_buf = StringIO(input)
+    out_buf = StringIO()
+    transform_file(handlers, transformer, path, in_buf, out_buf)
+    out_buf.seek(0)
+    return out_buf.read()
