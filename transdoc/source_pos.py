0860b39aa376ee7d1739bd782ebff215a4dba195
Maddy Guthridge <hello@maddyguthridge.com>
2025-01-25 00:16:56 +1100

    Add SourcePos static methods for zero positions

--- transdoc/source_pos.py
+++ transdoc/source_pos.py
@@ -3,6 +3,7 @@
 
 Definition for `SourcePos` class.
 """
+
 from dataclasses import dataclass
 
 
@@ -11,15 +12,24 @@ class SourcePos:
     """
     A position within a source file.
     """
+
     row: int
     """File row (1-indexed)"""
     col: int
     """File col (1-indexed)"""
 
+    @staticmethod
+    def zero() -> "SourcePos":
+        """
+        A zeroed source position. This indicates that an error associated with
+        the file as a whole, rather than at a particular position in the file.
+        """
+        return SourcePos(0, 0)
+
     def __str__(self) -> str:
         return f"{self.row}:{self.col}"
 
-    def __add__(self, other: 'SourcePos') -> 'SourcePos':
+    def __add__(self, other: "SourcePos") -> "SourcePos":
         if other.row == 1:
             # No change in row, offset column
             return SourcePos(self.row, self.col + other.col - 1)
@@ -27,16 +37,16 @@ class SourcePos:
             # Change in row, use other col
             return SourcePos(self.row + other.row - 1, other.col)
 
-    def offset_by_str(self, string: str) -> 'SourcePos':
+    def offset_by_str(self, string: str) -> "SourcePos":
         """
         Return a new `SourcePos` offset by the contents of the given string.
         """
-        row_offset = string.count('\n')
+        row_offset = string.count("\n")
 
         if row_offset == 0:
             return SourcePos(self.row, self.col + len(string))
         else:
-            last_row = string.rsplit('\n', 1)[-1]
+            last_row = string.rsplit("\n", 1)[-1]
             return SourcePos(self.row + row_offset, len(last_row) + 1)
 
 
@@ -47,7 +57,19 @@ class SourceRange:
 
     Range is from `start <= p < end`.
     """
+
     start: SourcePos
     """Start position"""
     end: SourcePos
     """End position (exclusive)"""
+
+    @staticmethod
+    def zero():
+        """
+        A zeroed source position. This indicates that an error associated with
+        the file as a whole, rather than at a particular position in the file.
+        """
+        return SourceRange(
+            SourcePos.zero(),
+            SourcePos.zero(),
+        )
